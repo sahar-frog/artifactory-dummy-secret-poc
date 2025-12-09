df1dc38a70d083b5266842790b0bf1347ac643e5
Sahar <sahars@jfrog.com>
2025-12-09 15:14:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 ARTIFACTORY_URL = "https://shredder.jfrog.io/artifactory/api/system/ping"
-TOKEN = os.getenv("cmVmdGtuOjAxOjE3OTYwMzQ1MjM6a1hQbE5qR3d6U2hYbU9jR1Z0dWJ6cE1Qd0k=")
+TOKEN = os.getenv("cmVmdA7kG2q9tQf8Xx1BzE4mR0sLpH3uVaC6wJ9NdS2yTgK5rQh8UuP0cMfB7DnZ")
 
 headers = {
     "Authorization": f"Bearer {TOKEN}"
